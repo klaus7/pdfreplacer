82df963b158e283a8dfa51c1470f4726e7aa01c8
Klaus Pfeiffer <klaus@allpiper.com>
2019-10-16 18:27:15 +0200

    added unit test

--- src/main/java/com/allpiper/pdfreplacer/util/ImageComparator.java
+++ src/main/java/com/allpiper/pdfreplacer/util/ImageComparator.java
@@ -1,5 +1,6 @@
-package com.allpiper.pdfreplacer;
+package com.allpiper.pdfreplacer.util;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -18,13 +19,13 @@ public class ImageComparator {
 
     public Result getDifference(boolean createImage) {
         final Result result = new Result();
-        // convert images to pixel arrays...
+        // convert images to pixel arrays
         final int w = img1.getWidth();
         final int h = img1.getHeight();
         result.totalPixelCount = w * h;
         final int[] p1 = img1.getRGB(0, 0, w, h, null, 0, w);
         final int[] p2 = img2.getRGB(0, 0, w, h, null, 0, w);
-        // compare img1 to img2, pixel by pixel. If different, highlight img1's pixel...
+        // compare pixel by pixel. If different, highlight img1's pixel.
         for (int i = 0; i < p1.length; i++) {
             if (p1[i] != p2[i]) {
                 p1[i] = highlight;
@@ -33,7 +34,7 @@ public class ImageComparator {
         }
         result.pixelDifferenceRelation = (float) result.pixelDifferenceCount / result.totalPixelCount;
         if (createImage) {
-            // save img1's pixels to a new BufferedImage, and return it...
+            // save img1's pixels to a new BufferedImage, and return it
             // (May require TYPE_INT_ARGB)
             result.out = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
             result.out.setRGB(0, 0, w, h, p1, 0, w);
@@ -42,6 +43,7 @@ public class ImageComparator {
     }
 
     @ToString
+    @Getter
     public static class Result {
         long totalPixelCount = 0;
         long pixelDifferenceCount = 0;
--- src/test/java/com/allpiper/pdfreplacer/CompareTest.java
+++ src/test/java/com/allpiper/pdfreplacer/CompareTest.java
@@ -1,25 +1,28 @@
 package com.allpiper.pdfreplacer;
 
+import com.allpiper.pdfreplacer.util.ImageComparator;
 import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.io.RandomAccessBuffer;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.junit.jupiter.api.Test;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /** @author Klaus Pfeiffer - klaus@allpiper.com */
 public class CompareTest {
 
-    public static void main(String[] args) throws Exception {
+    @Test
+    public void checkCorrectEmptyReplacement() throws Exception {
         PDFTextLocations locations = new PDFTextLocations();
         locations.add(new PDFTextSearchLocation("###FIELD1###"));
         locations.add(new PDFTextSearchLocation("###FIELD2###"));
 
-        String doc = "src/test/resources/" + "test";
-        File file = new File(doc + ".pdf");
-        PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+        PDFParser parser = new PDFParser(new RandomAccessBuffer(getClass().getResourceAsStream("/test.pdf")));
         parser.parse();
 
         try (COSDocument cosDoc = parser.getDocument()) {
@@ -30,17 +33,21 @@ public class CompareTest {
             locator.searchAndReplace();
 
             document.setAllSecurityToBeRemoved(true);
-            document.save(doc + "_processed.pdf");
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            document.save(output);
+            byte[] outputBytes = output.toByteArray();
             document.close();
+
+            // compare
+            BufferedImage empty = new PDFRenderer(PDDocument.load(getClass().getResourceAsStream("/test_empty.pdf"))).renderImageWithDPI(0, 300);
+            BufferedImage processed = new PDFRenderer(PDDocument.load(outputBytes)).renderImageWithDPI(0, 300);
+
+            ImageComparator imageComparator = new ImageComparator(empty, processed);
+            ImageComparator.Result difference = imageComparator.getDifference(false);
+            System.out.println(difference);
+
+            assertThat(difference.getPixelDifferenceCount()).isEqualTo(0);
         }
-
-        BufferedImage empty = new PDFRenderer(PDDocument.load(new File(doc + "_empty.pdf"))).renderImageWithDPI(0, 300);
-        BufferedImage processed = new PDFRenderer(PDDocument.load(new File(doc + "_processed.pdf"))).renderImageWithDPI(0, 300);
-        ImageComparator imageComparator = new ImageComparator(empty, processed);
-        ImageComparator.Result difference = imageComparator.getDifference(false);
-        System.out.println(difference);
-
-        assert difference.pixelDifferenceCount == 0;
     }
 
 
